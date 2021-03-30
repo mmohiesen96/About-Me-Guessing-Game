e4a35997d56fb10590cab59079673ca7d5bdaa12
mmohiesen96 <mmohiesen996@gmail.com>
2021-03-30 14:13:41 +0300

    question 6  & 7 added , top 10 eries added

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 let userName = prompt('Enter your name please ..');
-alert('Welcome' + userName + ', let\'s test you about me');
+alert('Welcome ' + userName + ', let\'s test you about me');
 
 let score = 0;
 
 let myName = prompt('is my name Mohammed ?');
 myName = myName.toLowerCase();
-if(myName=='y' || myName == 'yes'){
+if(myName==='y' || myName === 'yes'){
     alert('That is right i am mohammed nice to meet you');
     score++;
     // console.log('Correct answer');
@@ -33,6 +33,7 @@ else{
     // console.log('Inorrect answer');
     alert('Unfortunatly i like black');
 }
+
 
 
 
@@ -68,7 +69,7 @@ let myFavoriteHobby = prompt('Do i love cooooding "That is a hint" ?');
 myFavoriteHobby = myFavoriteHobby.toLowerCase();
 
 
-if(myFavoriteHobby=='y' || myFavoriteHobby == 'yes'){
+if(myFavoriteHobby ==='y' || myFavoriteHobby === 'yes'){
     alert('That is my biggest passion');
     score++;
     // console.log('Correct answer');
@@ -80,4 +81,52 @@ else{
     alert('OOOH no i gave you a hint');
 }
 
-alert('Welcome abroad, ' + userName + ' You scored ' + score + ' out of 5 at my personal information exam.')
+
+let myFavoriteNumber = Math.floor(Math.random() * 11);
+
+let userNumberGuess = 0;
+for (let i = 0 ; i <=4 ; i++){
+    if(i === 4) {
+        alert('You run out of attempts My favorite number  is '+ myFavoriteNumber);
+        break;
+    }
+    userNumberGuess = prompt('Can you guess my favorite number , I\'ll give you 4 attempts ');
+    if(parseInt(userNumberGuess) === myFavoriteNumber){
+        alert('Bingooo , you are correct my favorite number is ' + myFavoriteNumber);
+        score++;
+        break;
+    }
+    else if(userNumberGuess > myFavoriteNumber) {
+        alert('That is too high , try again');
+    }
+
+    else {
+        alert('That is too low , try again');
+    }
+
+}
+
+let myFavoriteProgrammingLanguage = ['Java' , 'C++' , 'Python' , 'C#' , 'JavaScript'];
+
+let userAnswerProgrammingLanguage = 'none';
+
+for(let i = 0 ; i < 7 ; i++){
+    if (i === 6 ) {
+        alert('You ran out of attempts , I love java and those are the possible answers will be displayed ');
+        break;
+    }
+    userAnswerProgrammingLanguage = prompt('Guess my favorite prgramming language i will give you 6 attempts "for hint it is an oop based language"');
+    if(userAnswerProgrammingLanguage.toLowerCase() === 'java'){
+        alert('That is correct such an elegant person you are');
+        score++;
+        break;
+    }
+
+    else {
+        alert('That is wrong try again');
+    }
+}
+alert('The options for the programming language question : ' + myFavoriteProgrammingLanguage);
+
+
+alert('Welcome abroad, ' + userName + ' You scored ' + score + ' out of 7 at my personal information quiz.');
